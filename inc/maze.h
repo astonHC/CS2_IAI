ab54cb9b5b48c6259c41721023c23fbbd1102461
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-12-12 15:38:16 +0000

    initial maze commit

---
+++ inc/maze.h
@@ -0,0 +1,22 @@
+/* COPYRIGHT (C) HARRY CLARK 2024 */
+/* CS2_IAI STUDY MATERIAL */
+
+/* THIS FILE PERTAINS TOWARDS THE DECLARATION TO THE METHODS SURROUNDING THE SEARCHING SUBJECT */
+
+#ifndef _MAZE_
+#define _MAZE_
+
+/* SYSTEM INCLUDES */
+
+#include <stdio.h>
+
+static int MAZE[5][5] = 
+{
+    {   0, 0, 0, 0, 0   },
+    {   0, 1, 0, 0, 0   },
+    {   1, 1, 0, 0, 0   },
+    {   1, 1, 1, 0, 0   },
+    {   1, 1, 1, 1, 0   }
+};
+
+#endif
